be04a2d73dc8e645957adfafd4f6ba02965ea55f
Daniel Herschel <76445392+DanielHerschel@users.noreply.github.com>
2024-09-09 22:01:48 +0300

    Added generated API

---
+++ internal/server/routes.go
@@ -0,0 +1,40 @@
+package server
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (s *Server) RegisterRoutes() http.Handler {
+	r := httprouter.New()
+	r.HandlerFunc(http.MethodGet, "/", s.HelloWorldHandler)
+
+	r.HandlerFunc(http.MethodGet, "/health", s.healthHandler)
+
+	return r
+}
+
+func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	resp := make(map[string]string)
+	resp["message"] = "Hello World"
+
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("error handling JSON marshal. Err: %v", err)
+	}
+
+	_, _ = w.Write(jsonResp)
+}
+
+func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
+	jsonResp, err := json.Marshal(s.db.Health())
+
+	if err != nil {
+		log.Fatalf("error handling JSON marshal. Err: %v", err)
+	}
+
+	_, _ = w.Write(jsonResp)
+}
